fcc61dc8b932d43e310f7572830a4663b582b54d
AmulyaMachhan <machhanamulya@gmail.com>
2025-12-22 01:36:35 +0530

    Added reddit handler

--- packages/core/src/platforms/index.ts
+++ packages/core/src/platforms/index.ts
@@ -1,11 +1,7 @@
-import { linkedinHandler } from "./linkedin";
-import { unknownHandler } from "./unknown";
-import { youtubeHandler } from "./youtube";
-import { instagramHandler } from "./instagram";
+import { linkedinHandler } from './linkedin';
+import { unknownHandler } from './unknown';
+import { youtubeHandler } from './youtube';
+import { instagramHandler } from './instagram';
+import { redditHandler } from './reddit';
 
-export {
-    linkedinHandler,
-    youtubeHandler,
-    instagramHandler,
-    unknownHandler
-}
+export { linkedinHandler, youtubeHandler, instagramHandler, redditHandler, unknownHandler };
